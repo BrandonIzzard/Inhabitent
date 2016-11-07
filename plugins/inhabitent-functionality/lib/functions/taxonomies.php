2e406144ccdfa166fb15ff0c1ac3a47032d55d36
Brandon Izzard <xbrandoni@gmail.com>
2016-11-07 13:13:10 -0800

    custom taxonomies

--- plugins/inhabitent-functionality/lib/functions/taxonomies.php
+++ plugins/inhabitent-functionality/lib/functions/taxonomies.php
@@ -6,3 +6,41 @@
  */
 
 // Add your custom taxonomies here...
+// Register Custom Taxonomy
+function register_poduct_type_taxonomy() {
+
+	$labels = array(
+		'name'                       => 'Products Types',
+		'singular_name'              => 'Product Type',
+		'menu_name'                  => 'Product Types',
+		'all_items'                  => 'All Product Types',
+		'parent_item'                => 'Parent Product Type',
+		'parent_item_colon'          => 'Parent Product Type:',
+		'new_item_name'              => 'New Product Type Name',
+		'add_new_item'               => 'Add Product Type Item',
+		'edit_item'                  => 'Edit Product Type',
+		'update_item'                => 'Update Product Type',
+		'view_item'                  => 'View Product Type',
+		'separate_items_with_commas' => 'Separate product types with commas',
+		'add_or_remove_items'        => 'Add or remove product types',
+		'choose_from_most_used'      => 'Choose from the most used',
+		'popular_items'              => 'Popular Product Types',
+		'search_items'               => 'Search Product Types',
+		'not_found'                  => 'Not Found',
+		'no_terms'                   => 'No product types',
+		'items_list'                 => 'Product types list',
+		'items_list_navigation'      => 'Product types list navigation',
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'product_type', array( 'post' ), $args );
+
+}
+add_action( 'init', 'register_poduct_type_taxonomy', 0 );
