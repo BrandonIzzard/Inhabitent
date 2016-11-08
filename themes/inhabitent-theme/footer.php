d5a406430a01fc4bea78d89214850aa87599ec47
Brandon Izzard <xbrandoni@gmail.com>
2016-11-07 16:46:10 -0800

    front-page and fixed imgs

--- themes/inhabitent-theme/footer.php
+++ themes/inhabitent-theme/footer.php
@@ -10,10 +10,10 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-						<div class ="footer-left"><!--contact & business hours-->
+						<div class ="footer-left">
 								<div class="contact-us">
 									<h3>Contact Us</h3>
-										<p><i class="fa fa-envelope" aria-hidden="true"></i> info@inhabitent.com</p>
+										<p><i class="fa fa-envelope" aria-hidden="true"></i> Info@Inhabitent.com</p>
 										<p><i class="fa fa-phone" aria-hidden="true"></i> 604-604-6044</p>
 										<p><i class="fa fa-facebook-square" aria-hidden="true"></i> <i class="fa fa-twitter-square" aria-hidden="true"></i> <i class="fa fa-google-plus-square" aria-hidden="true"></i></p>
 								</div>
@@ -24,9 +24,10 @@
 										<p>Sunday: Closed</p>
 								</div>
 								<div class = "footer-logo"><!--logo-->
-								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="logo">
+							
+
 						<img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-text.svg'; ?>" class="logo" alt="Inhabitent logo" />
-					</a>
+
 							</div><!-- end of footer-logo -->
 							</div><!--end of footer left -->
 							
