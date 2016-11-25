df06dfbc6bda0174bc09234789508e0983fbba49
Brandon Izzard <xbrandoni@gmail.com>
2016-11-24 16:21:55 -0800

    font awesome

--- themes/inhabitent-theme/functions.php
+++ themes/inhabitent-theme/functions.php
@@ -84,7 +84,7 @@ add_filter( 'stylesheet_uri', 'Inhabitent_minified_css', 10, 2 );
  */
 function Inhabitent_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
-	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'); // Adding Font Awesome.
+	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'); // Adding Font Awesome.
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script(
